643651acb1d8c5ec151e6e86d5e5fde93912ec6e
Tiffany <tiffwu1027@gmail.com>
2021-04-10 19:18:18 -0700

    created functional predict endpoint using model, including uploading the image from post

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, request
+from keras.models import load_model
+import cv2
+import numpy as np
+import random
+import os
+from keras.preprocessing.image import ImageDataGenerator
 
 app = Flask(__name__)
+
+model = load_model("./model/model_keras.h5")
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+def predict_single(img_path):
+    img = np.array(cv2.resize(cv2.imread(img_path, cv2.IMREAD_COLOR), (150,150), interpolation=cv2.INTER_CUBIC)).reshape((1, 150, 150, 3))
+    return model.predict(img)
+
+def decode_labels(target, thresh=0.5):
+    labels = {
+        0: ['crimp', 'crimpy boi', 'rip fingers'],
+        1: ['jug', 'victory jug'],
+        2: ['sloper', 'sloppy', 'sloppy boi'],
+        3: ['pinch', 'piiiinch', 'pinchy boi'],
+        4: ['pocket', 'not a sloper'],
+        5: ['edge']
+    }
+    return random.choice(labels[np.argmax(target)])
+
+
+
+"""
+ENDPOINTS BELOW
+"""
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        print(request.files)
+        f = request.files["file"]
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, "uploads", f.filename)
+        f.save(file_path)
+
+        preds = predict_single(file_path)
+        result = decode_labels(preds)
+        print(result)
+        return result
+    return None
 
 
 if __name__ == '__main__':
